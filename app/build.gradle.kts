25ae415c4d0259e7d10a62e57f2bef2dd326bb31
chrimast <119728939+chrimast@users.noreply.github.com>
2026-08-12 23:16:11 +0800

    release VPStool 1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.sekusarisu.yanami"
         minSdk = 28
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.1.0"
+        versionCode = 10
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
